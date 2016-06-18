0c3889336795ae01c13de285806228fdb92776c7
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2016-06-18 22:36:51 +0400

    link the content of the app for burj khalifa 2

--- app/src/main/java/com/jiahaoliuliu/dubaitouristguide/TouristAttraction.java
+++ app/src/main/java/com/jiahaoliuliu/dubaitouristguide/TouristAttraction.java
@@ -2,6 +2,8 @@ package com.jiahaoliuliu.dubaitouristguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class TouristAttraction extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class TouristAttraction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tourist_attraction);
+
+        TextView title = (TextView) findViewById(R.id.title);
+        title.setText("Burj Khalifa 2");
+
+        ImageView image = (ImageView) findViewById(R.id.image);
+        image.setImageDrawable(getResources().getDrawable(R.drawable.burj_khalifa_2));
     }
 }
